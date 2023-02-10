e7ab9e77a4999d89d620ccad1657a7ec5180f110
SokaLL <SokaLL@users.noreply.github.com>
2023-02-10 11:04:09 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,27 @@
-basic.forever(function () {
-    basic.showString("a")
+function doMotor () {
+    if (true) {
+    	
+    }
+}
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    connect = true
     basic.clearScreen()
-    basic.showArrow(ArrowNames.North)
+    while (connect) {
+        basic.showArrow(ArrowNames.North)
+        basic.clearScreen()
+    }
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Sad)
+    connect = false
+})
+let connect = false
+bluetooth.setTransmitPower(7)
+bluetooth.startUartService()
+basic.showIcon(IconNames.Heart)
+connect = false
+basic.forever(function () {
+	
 })
